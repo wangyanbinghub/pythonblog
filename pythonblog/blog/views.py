beabfbfaea286e6efac43bf4176b90fb83cba340
wangyanbinghub <wangyanbinoracle@163.com>
2017-09-29 14:41:14 +0800

    m view

--- pythonblog/blog/views.py
+++ pythonblog/blog/views.py
@@ -1,6 +1,13 @@
-#blog/views
+#blog/views.py
+#-*- coding:utf-8 -*-
+from django.shortcuts import render
+
+# Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("欢迎访问我的博客首页")
+    return render(request, 'blog/index.html', context={
+        'title':'我的博客首页',
+        'welcome':'欢迎访问我的博客首页'
+    })
